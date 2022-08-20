b031f1d77a7dafc51c916b8ebab9299ed98a19e8
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-08-20 07:22:33 +0200

    styles components

---
+++ src/components/Header.components.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from "@styles/components/Header.styles";
+
+const Header = () => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/styles/components/Header.styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Container = styled.h1`
+  font-size: 18px;
+  color: #2A2A2A;
+  font-weight: 400;
+`;
